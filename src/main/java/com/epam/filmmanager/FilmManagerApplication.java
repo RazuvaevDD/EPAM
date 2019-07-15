cc09b5080ac07e9534eabb81bc705e584e9d755b
Razuvaev_DD <Razuvaev_DD@mail.ru>
2019-07-15 05:43:40 +0000

    Added api-request, parsing json and Film's blocks.

--- src/main/java/com/epam/filmmanager/FilmManagerApplication.java
+++ src/main/java/com/epam/filmmanager/FilmManagerApplication.java
@@ -3,11 +3,16 @@ package com.epam.filmmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.json.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class FilmManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FilmManagerApplication.class, args);
+		
 	}
-
 }
